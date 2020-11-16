7b76806273a413bc57d3fda9c3c6c7a881173814
Ion Leahu <forza11879@gmail.com>
2020-11-16 17:51:30 -0500

    project init

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotnev from 'dotenv';
+import { createServer } from 'http';
+
+const app = express();
+
+dotnev.config({ path: './config/dev.env' });
+const port = process.env.PORT;
+
+const server = createServer(app);
+server.listen(port, function () {
+  console.log(`Server is up on port ${port}`);
+});
